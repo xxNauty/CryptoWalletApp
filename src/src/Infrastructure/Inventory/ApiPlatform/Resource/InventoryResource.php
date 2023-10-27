88ef119cbb1f70421d4a4b6ba068d113b8af7f2b
Mateusz Wnuk <mateusz.wnuk@bandit.house>
2023-10-27 11:08:37 +0200

    softDeletableTrait

--- src/src/Infrastructure/Inventory/ApiPlatform/Resource/InventoryResource.php
+++ src/src/Infrastructure/Inventory/ApiPlatform/Resource/InventoryResource.php
@@ -5,15 +5,6 @@ declare(strict_types=1);
 namespace App\Infrastructure\Inventory\ApiPlatform\Resource;
 
 use ApiPlatform\Metadata\ApiProperty;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Delete;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
-use App\Infrastructure\Inventory\ApiPlatform\State\Processor\InventoryCrudProcessor;
-use App\Infrastructure\Inventory\ApiPlatform\State\Provider\InventoryCrudProvider;
 use App\Infrastructure\Shared\ApiPlatform\Resource\ResourceFactory;
 use App\Infrastructure\Shared\ApiPlatform\Resource\ResourceInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
